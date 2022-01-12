8d412efe964f5f783e2e028a856b268eb201a9c8
imPiyushrRajput <piyushrajput1519@gamil.com>
2022-01-12 20:10:41 +0530

    fix dyanmic content issue

--- src/pages/projects/[project].js
+++ src/pages/projects/[project].js
@@ -3,30 +3,47 @@ import { jsx } from "theme-ui";
 import Layout from "components/layout";
 import PageTitle from "components/PageTitle";
 import Project from "components/Project";
-import { useRouter } from "next/router";
-import { projects } from "constants/projects";
+import { projects as StaticProjects } from "constants/projects";
 
-const Services = () => {
-  const router = useRouter();
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      project: StaticProjects.find(
+        (project) =>
+          project.title.split("/").join(" ").toLowerCase() ===
+          params.project.split("-").join(" ").toLowerCase()
+      ),
+    },
+    revalidate: 1,
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = StaticProjects.map((project) => ({
+    params: {
+      project: project.title.toLowerCase().split(" ").join("-"),
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const Projects = ({ project }) => {
   return (
     <Layout>
       <PageTitle
         title="Projects"
         text="Explore our wide range of Projects delivered"
       />
-      {router.query.project && (
-        <Project
-          data={projects.find(
-            (project) =>
-              project.title.split("/").join(" ").toLowerCase() ===
-              router.query.project.split("-").join(" ").toLowerCase()
-          )}
-          backgroundColor="backgroundSecondary"
-          flexDirection="row"
-        />
-      )}
+      <Project
+        data={project}
+        backgroundColor="backgroundSecondary"
+        flexDirection="row"
+      />
     </Layout>
   );
 };
 
-export default Services;
+export default Projects;
--- src/pages/services/[service].js
+++ src/pages/services/[service].js
@@ -2,26 +2,46 @@
 import { jsx } from "theme-ui";
 import Layout from "components/layout";
 import PageTitle from "components/PageTitle";
-import { services } from "constants/services";
+import { services as StaticServices } from "constants/services";
 import Service from "components/Service";
-import { useRouter } from "next/router";
 
-const Services = () => {
-  const router = useRouter();
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      service: StaticServices.find(
+        (service) =>
+          service.title.split("/").join(" ").toLowerCase() ===
+          params.service.split("-").join(" ").toLowerCase()
+      ),
+    },
+    revalidate: 1,
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = StaticServices.map((service) => ({
+    params: {
+      service:
+        service.title === "UI/UX"
+          ? service.title.toLowerCase().split("/").join("-")
+          : service.title.toLowerCase().split(" ").join("-"),
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const Services = ({ service }) => {
   return (
     <Layout>
       <PageTitle title="Services" text="Explore our wide range of Services" />
-      {router.query.service && (
-        <Service
-          data={services.find(
-            (service) =>
-              service.title.split("/").join(" ").toLowerCase() ===
-              router.query.service.split("-").join(" ").toLowerCase()
-          )}
-          backgroundColor="backgroundSecondary"
-          flexDirection="row"
-        />
-      )}
+      <Service
+        data={service}
+        backgroundColor="backgroundSecondary"
+        flexDirection="row"
+      />
     </Layout>
   );
 };
