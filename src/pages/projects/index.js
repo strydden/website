cdc0b91af5f68a88410dc0538b7de1f01d5dd03f
Abg4real <abhyag126@gmail.com>
2023-05-02 23:00:45 +0530

    AG | Add masjid data

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -6,26 +6,31 @@ import Project from "components/Project";
 import { projects } from "constants/projects";
 
 const Projects = () => {
-  return (
-    <Layout>
-      <PageTitle
-        title="Projects"
-        text="Explore our wide range of Projects delivered"
-      />
-      <Project
-        data={projects[0]}
-        backgroundColor="backgroundSecondary"
-        flexDirection="row"
-      />
-      <Project data={projects[1]} flexDirection="row-reverse" />
-      <Project
-        data={projects[2]}
-        backgroundColor="backgroundSecondary"
-        flexDirection="row"
-      />
-      <Project data={projects[3]} flexDirection="row-reverse" />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <PageTitle
+                title="Projects"
+                text="Explore our wide range of Projects delivered"
+            />
+            <Project
+                data={projects[0]}
+                backgroundColor="backgroundSecondary"
+                flexDirection="row"
+            />
+            <Project data={projects[1]} flexDirection="row-reverse" />
+            <Project
+                data={projects[2]}
+                backgroundColor="backgroundSecondary"
+                flexDirection="row"
+            />
+            <Project data={projects[3]} flexDirection="row-reverse" />
+            <Project
+                data={projects[4]}
+                flexDirection="row"
+                backgroundColor="backgroundSecondary"
+            />
+        </Layout>
+    );
 };
 
 export default Projects;
